6f525c0dace9f4ae2b69a4e5f0d1e6de86ae3084
Kajetan Ziółkowski <kajetan.ziolkowsky@gmail.com>
2026-05-21 16:41:38 +0200

    uuge cleanup + snapper for docs

--- modules/hosts/pyro/pyro-hardware.nix
+++ modules/hosts/pyro/pyro-hardware.nix
@@ -295,11 +295,27 @@
       services.snapper = {
         snapshotInterval = "hourly";
         cleanupInterval = "1d";
+        
         configs.home = {
           SUBVOLUME = "/home";
           FSTYPE = "btrfs";
+          
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
+          
+          TIMELINE_LIMIT_HOURLY = "8";
+          TIMELINE_LIMIT_DAILY = "6";
+          TIMELINE_LIMIT_WEEKLY = "4";
+          TIMELINE_LIMIT_MONTHLY = "2";
+          TIMELINE_LIMIT_YEARLY = "1";
+        };
+        configs.dokumenty = {
+          SUBVOLUME = "/mnt/Barracuda/@dokumenty";
+          FSTYPE = "btrfs";
+          
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          
           TIMELINE_LIMIT_HOURLY = "8";
           TIMELINE_LIMIT_DAILY = "6";
           TIMELINE_LIMIT_WEEKLY = "4";
@@ -314,6 +330,7 @@
         fileSystems = [
           "/"
           "/mnt/Barracuda"
+          "/mnt/WDigital"
         ];
       };
 
